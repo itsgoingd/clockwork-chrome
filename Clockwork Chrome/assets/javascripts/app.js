d6d2d764b90ff3757299f8ad40cfd1552851520a
tchiotludo <tchiot.ludo@gmail.com>
2016-01-22 17:59:53 +0100

    Add a memory on main panel

--- Clockwork Chrome/assets/javascripts/app.js
+++ Clockwork Chrome/assets/javascripts/app.js
@@ -94,6 +94,15 @@ var Clockwork = angular.module('Clockwork', [])
 			 return value? input[0] : input[1];
 		 };
 	 })
+	.filter('bytes', function() {
+		return function(bytes, precision) {
+			if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+			if (typeof precision === 'undefined') precision = 1;
+			var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+				number = Math.floor(Math.log(bytes) / Math.log(1024));
+			return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+		}
+	})
 	.directive('resizableColumns', function ($parse) {
 		return {
 			link: function (scope, element, attrs) {
