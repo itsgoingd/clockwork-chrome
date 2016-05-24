5d79d5fec61266d318913f52ebc92e437f06e637
its <itsgoingd@luzer.sk>
2016-05-24 20:00:21 +0200

    Removed some unused code and debugging output.

--- Clockwork Chrome/assets/javascripts/background.js
+++ Clockwork Chrome/assets/javascripts/background.js
@@ -17,12 +17,3 @@ function onRequest(request, sender, callback) {
 	}
 }
 chrome.extension.onRequest.addListener(onRequest);
-
-// var devtoolsPort = null;
-// chrome.extension.onConnect.addListener(function(port) {
-// 	devtoolsPort = port;
-// });
-
-// chrome.extension.onMessage.addListener(function(request, sender, sendResponse){
-// 	devtoolsPort.postMessage({});
-// });
