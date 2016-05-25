2e16553fb76c61d5d15f9104a0716d2447831dcd
its <itsgoingd@luzer.sk>
2016-05-25 14:46:39 +0200

    Added support for context in the log tab.

--- Clockwork Chrome/assets/javascripts/panel.js
+++ Clockwork Chrome/assets/javascripts/panel.js
@@ -307,6 +307,7 @@ Clockwork.controller('PanelController', function($scope, $http, toolbar)
 
 		$.each(data, function(key, value) {
 			value.time = new Date(value.time * 1000);
+			value.context = value.context !== '[]' ? value.context : undefined;
 			value.fullPath = value.file && value.line ? value.file.replace(/^\//, '') + ':' + value.line : undefined;
 			value.shortPath = value.fullPath ? value.fullPath.split(/[\/\\]/).pop() : undefined;
 		});
