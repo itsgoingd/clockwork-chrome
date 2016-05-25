face16124451a6f8d3e672d3876b5d73d550301d
its <itsgoingd@luzer.sk>
2016-05-25 16:45:18 +0200

    Use XMLHttpRequest directly for requesting Clockwork metadata to avoid requiring jquery in the background page.

--- Clockwork Chrome/assets/javascripts/background.js
+++ Clockwork Chrome/assets/javascripts/background.js
@@ -1,19 +1,25 @@
 function onRequest(request, sender, callback) {
 	if (request.action == 'getJSON') {
-		$.ajax({
-            url: request.url,
-            type: 'GET',
-            dataType: 'json',
-            success: callback,
-            error: function(error){
-                console.log('ERROR'); console.log(error);
-            },
-            beforeSend: function (xhr) {
-                $.each(request.headers, function(headerName, headerValue) {
-                    xhr.setRequestHeader(headerName, headerValue);
-                });
-            }
-        });
+		var xhr = new XMLHttpRequest();
+
+		xhr.open('GET', request.url, true);
+
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				if (xhr.status == 200) {
+					callback(JSON.parse(xhr.responseText));
+				} else {
+					console.log('Error getting Clockwork metadata:');
+					console.log(xhr.responseText);
+				}
+			}
+		}
+
+		Object.keys(request.headers).forEach(function(headerName) {
+		    xhr.setRequestHeader(headerName, request.headers[headerName]);
+		});
+
+		xhr.send();
 	}
 }
 chrome.extension.onRequest.addListener(onRequest);
